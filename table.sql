a159ed6e042624439600c22106c4e55be806e801
180107092 <55088565+180107092@users.noreply.github.com>
2020-04-24 23:22:20 +0600

    Add files via upload

---
+++ table.sql
@@ -0,0 +1,22 @@
+CREATE TABLE Customers (
+ID CHAR(10) NOT NULL Primary key,
+Name CHAR(25) NOT NULL,
+Last_name CHAR(25) NULL,
+City CHAR(25) NULL,
+Phone CHAR(25) NOT NULL, DEFAULT NOWO)
+
+INSERT INTO Sellers (ID, Name, Last_name, City, Phone,) 
+VALUES  ('1', 'Erkin', 'ABdikarimov', 'Qyzylorda', '8 777 777 77 77'),
+		('2', 'Arman', 'Kasym', 'Almaty', '8 777 777 77 77')
+		('3', 'Kasym', 'Erbol', 'Taraz', '8 777 777 77 77')
+		('4', 'Erbol', 'Asylbek', 'Taldykorgan', '8 777 777 77 77')
+		('5', 'Asylbek', 'Amanzhol', 'Shymkent', '8 777 777 77 77')
+		('6', 'Amanzhol', 'Zhantore', 'Oral', '8 777 777 77 77')
+		('7', 'Zhantore', 'Zhonart', 'Karagandy', '8 777 777 77 77')
+		('8', 'Zhonart', 'Nurgasur', 'Kokshetau', '8 777 777 77 77')
+		('9', 'Nurgasur', 'Adilzham', 'Zhesgazgan', '8 777 777 77 77')
+		('10', 'Adilzham', 'Aidar', 'Mangystau', '8 777 777 77 77')
+		('11', 'Aidar', 'Serik', 'Aktobe', '8 777 777 77 77')
+		('12', 'Serik', 'Kadyr', 'Atyrau', '8 777 777 77 77')
+		('13', 'Kadyr', 'Esbol', 'Aktau', '8 777 777 77 77')
+		('14', 'Ulzhan', 'Baitas', 'Qyzylorda', '8 777 777 77 77')
